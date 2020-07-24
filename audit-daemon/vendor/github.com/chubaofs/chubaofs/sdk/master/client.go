3987f5d562eb76d29edc7b737621a02f73165ce0
wenjia322 <buaa1214wwj@126.com>
2020-07-24 14:57:01 +0800

    fix: add import vendor
    Signed-off-by: wenjia322 <buaa1214wwj@126.com>

---
+++ audit-daemon/vendor/github.com/chubaofs/chubaofs/sdk/master/client.go
@@ -0,0 +1,275 @@
+// Copyright 2018 The Chubao Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package master
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/chubaofs/chubaofs/proto"
+	"github.com/chubaofs/chubaofs/util/log"
+)
+
+const (
+	requestTimeout = 30 * time.Second
+)
+
+var (
+	ErrNoValidMaster = errors.New("no valid master")
+)
+
+type MasterClient struct {
+	sync.RWMutex
+	masters    []string
+	useSSL     bool
+	leaderAddr string
+	timeout    time.Duration
+
+	adminAPI  *AdminAPI
+	clientAPI *ClientAPI
+	nodeAPI   *NodeAPI
+	userAPI   *UserAPI
+}
+
+// AddNode add the given address as the master address.
+func (c *MasterClient) AddNode(address string) {
+	c.Lock()
+	c.updateMaster(address)
+	c.Unlock()
+}
+
+// Leader returns the current leader address.
+func (c *MasterClient) Leader() (addr string) {
+	c.RLock()
+	addr = c.leaderAddr
+	c.RUnlock()
+	return
+}
+
+func (c *MasterClient) AdminAPI() *AdminAPI {
+	return c.adminAPI
+}
+
+func (c *MasterClient) ClientAPI() *ClientAPI {
+	return c.clientAPI
+}
+
+func (c *MasterClient) NodeAPI() *NodeAPI {
+	return c.nodeAPI
+}
+
+func (c *MasterClient) UserAPI() *UserAPI {
+	return c.userAPI
+}
+
+// Change the leader address.
+func (c *MasterClient) setLeader(addr string) {
+	c.Lock()
+	c.leaderAddr = addr
+	c.Unlock()
+}
+
+// Change the request timeout
+func (c *MasterClient) SetTimeout(timeout uint16) {
+	c.Lock()
+	c.timeout = time.Duration(timeout) * time.Second
+	c.Unlock()
+}
+
+func (c *MasterClient) serveRequest(r *request) (repsData []byte, err error) {
+	leaderAddr, nodes := c.prepareRequest()
+	host := leaderAddr
+	for i := -1; i < len(nodes); i++ {
+		if i == -1 {
+			if host == "" {
+				continue
+			}
+		} else {
+			host = nodes[i]
+		}
+		var resp *http.Response
+		var schema string
+		if c.useSSL {
+			schema = "https"
+		} else {
+			schema = "http"
+		}
+		var url = fmt.Sprintf("%s://%s%s", schema, host,
+			r.path)
+		resp, err = c.httpRequest(r.method, url, r.params, r.header, r.body)
+		if err != nil {
+			log.LogErrorf("serveRequest: send http request fail: method(%v) url(%v) err(%v)", r.method, url, err)
+			continue
+		}
+		stateCode := resp.StatusCode
+		repsData, err = ioutil.ReadAll(resp.Body)
+		_ = resp.Body.Close()
+		if err != nil {
+			log.LogErrorf("serveRequest: read http response body fail: err(%v)", err)
+			continue
+		}
+		switch stateCode {
+		case http.StatusForbidden:
+			curMasterAddr := strings.TrimSpace(string(repsData))
+			curMasterAddr = strings.Replace(curMasterAddr, "\n", "", -1)
+			if len(curMasterAddr) == 0 {
+				log.LogWarnf("serveRequest: server response status 403: request(%s) status"+
+					"(403), body is empty", host)
+				err = ErrNoValidMaster
+				return
+			}
+			repsData, err = c.serveRequest(r)
+			return
+		case http.StatusOK:
+			if leaderAddr != host {
+				c.setLeader(host)
+			}
+			var body = &struct {
+				Code int32           `json:"code"`
+				Msg  string          `json:"msg"`
+				Data json.RawMessage `json:"data"`
+			}{}
+			if err := json.Unmarshal(repsData, body); err != nil {
+				log.LogErrorf("unmarshal response body err:%v", err)
+				return nil, fmt.Errorf("unmarshal response body err:%v", err)
+
+			}
+			// o represent proto.ErrCodeSuccess
+			if body.Code != 0 {
+				log.LogErrorf("serveRequest: code[%v], msg[%v], data[%v] ", body.Code, body.Msg, body.Data)
+				return nil, proto.ParseErrorCode(body.Code)
+			}
+			return []byte(body.Data), nil
+		default:
+			log.LogErrorf("serveRequest: unknown status: host(%v) uri(%v) status(%v) body(%s).",
+				resp.Request.URL.String(), host, stateCode, strings.Replace(string(repsData), "\n", "", -1))
+			continue
+		}
+	}
+	err = ErrNoValidMaster
+	return
+}
+
+// Nodes returns all master addresses.
+func (c *MasterClient) Nodes() (nodes []string) {
+	c.RLock()
+	nodes = c.masters
+	c.RUnlock()
+	return
+}
+
+// prepareRequest returns the leader address and all master addresses.
+func (c *MasterClient) prepareRequest() (addr string, nodes []string) {
+	c.RLock()
+	addr = c.leaderAddr
+	nodes = c.masters
+	c.RUnlock()
+	return
+}
+
+func (c *MasterClient) httpRequest(method, url string, param, header map[string]string, reqData []byte) (resp *http.Response, err error) {
+	client := http.DefaultClient
+	reader := bytes.NewReader(reqData)
+	if header["isTimeOut"] != "" {
+		var isTimeOut bool
+		if isTimeOut, err = strconv.ParseBool(header["isTimeOut"]); err != nil {
+			return
+		}
+		if isTimeOut {
+			client.Timeout = c.timeout
+		}
+	} else {
+		client.Timeout = c.timeout
+	}
+	var req *http.Request
+	fullUrl := c.mergeRequestUrl(url, param)
+	log.LogDebugf("httpRequest: merge request url: method(%v) url(%v) bodyLength[%v].", method, fullUrl, len(reqData))
+	if req, err = http.NewRequest(method, fullUrl, reader); err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Connection", "close")
+	for k, v := range header {
+		req.Header.Set(k, v)
+	}
+	resp, err = client.Do(req)
+	return
+}
+
+func (c *MasterClient) updateMaster(address string) {
+	contains := false
+	for _, master := range c.masters {
+		if master == address {
+			contains = true
+			break
+		}
+	}
+	if !contains {
+		c.masters = append(c.masters, address)
+	}
+	c.leaderAddr = address
+}
+
+func (c *MasterClient) mergeRequestUrl(url string, params map[string]string) string {
+	if params != nil && len(params) > 0 {
+		buff := bytes.NewBuffer([]byte(url))
+		isFirstParam := true
+		for k, v := range params {
+			if isFirstParam {
+				buff.WriteString("?")
+				isFirstParam = false
+			} else {
+				buff.WriteString("&")
+			}
+			buff.WriteString(k)
+			buff.WriteString("=")
+			buff.WriteString(v)
+		}
+		return buff.String()
+	}
+	return url
+}
+
+// NewMasterHelper returns a new MasterClient instance.
+func NewMasterClient(masters []string, useSSL bool) *MasterClient {
+	var mc = &MasterClient{masters: masters, useSSL: useSSL, timeout: requestTimeout}
+	mc.adminAPI = &AdminAPI{mc: mc}
+	mc.clientAPI = &ClientAPI{mc: mc}
+	mc.nodeAPI = &NodeAPI{mc: mc}
+	mc.userAPI = &UserAPI{mc: mc}
+	return mc
+}
+
+// NewMasterClientFromString parse raw master address configuration
+// string and returns a new MasterClient instance.
+// Notes that a valid format raw string must match: "{HOST}:{PORT},{HOST}:{PORT}"
+func NewMasterClientFromString(masterAddr string, useSSL bool) *MasterClient {
+	var masters = make([]string, 0)
+	for _, master := range strings.Split(masterAddr, ",") {
+		master = strings.TrimSpace(master)
+		if master != "" {
+			masters = append(masters, master)
+		}
+	}
+	return NewMasterClient(masters, useSSL)
+}
